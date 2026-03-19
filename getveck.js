5d2cf6ce60874ffb0c9a8e8748e8c4482346b746
suppressfinallygotgh <suppressgithub@proton.me>
2026-03-19 00:22:22 -0500

    Update getveck.js

--- getveck.js
+++ getveck.js
@@ -1,4 +1,16 @@
-const URL = "https://cdn.jsdelivr.net/gh/suppressfinallygotgh/smashkarts@main/veck.js?1=" + Date.now();
+const veckurl = "https://cdn.jsdelivr.net/gh/suppressfinallygotgh/smashkarts@main/veck.js?1=" + Date.now();
+const smashkartsurl = "https://cdn.jsdelivr.net/gh/suppressfinallygotgh/smashkarts@main/main.js?1=" + Date.now();
+
+const website = document.location.host
+let URL
+switch (website) {
+  case "https://veck.io":
+    URL = veckurl
+    break;
+  case "https://smashkarts.io":
+    URL = smashkartsurl
+    break;
+}
 
 fetch(URL)
   .then(r => r.text())
